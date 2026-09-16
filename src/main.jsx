cc815a48c65ec32a6f1a9c688f379b0f2b2da87d
DavidCastillo <davidcas3008@gmail.com>
2026-09-16 17:34:55 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -414,13 +414,7 @@ function App() {
                     <span>22 años</span>
                   </div>
 
-                  <div className="hero-photo-square">
-                    <span className="square-corner corner-tl" />
-                    <span className="square-corner corner-tr" />
-                    <span className="square-corner corner-br" />
-                    <span className="square-corner corner-bl" />
-                    <span className="square-scan-line" />
-
+                  <div className="hero-photo-core">
                     <div className="hero-photo-halo" />
                     <div className="hero-photo-inner">
                       <img
